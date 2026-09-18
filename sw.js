aaf09e477417d3d8e3e022c7e7d84bd3fe47961c
stewartdonjuan1-oss <stewartdonjuan1@gmail.com>
2026-09-18 22:22:45 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'tuckshop-invoice-v2';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'tuckshop-invoice-v3';
+const ASSETS = ['./', './index.html', './jspdf.umd.min.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
